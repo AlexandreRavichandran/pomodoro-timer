37b662e213fa0d1e4d10d2a091399cade503ad71
Alexandre RAVICHANDRAN <alexandre.ravichandran@gmail.com>
2021-06-18 09:02:41 +0200

    Add Heroku  database link

--- Pomodoro.php
+++ Pomodoro.php
@@ -14,7 +14,9 @@ class Pomodoro
     private function dbconnect()
     {
         try {
-            $db = new PDO('mysql:host=localhost;dbname=pomodoro-timer;charset=utf8', 'root', 'root');
+            //mysql://b70301025e38da:15bca50c@us-cdbr-east-04.cleardb.com/heroku_e3143e370808540?reconnect=true
+            //$db = new PDO('mysql:host=localhost;dbname=pomodoro-timer;charset=utf8', 'root', 'root');
+            $db = new PDO('mysql:host=us-cdbr-east-04.cleardb.com;dbname=heroku_e3143e370808540;charset=utf8', 'b70301025e38da', '15bca50c');
         } catch (Exception $e) {
             echo 'Une erreur s\'est produite' . $e->getMessage();
         }
